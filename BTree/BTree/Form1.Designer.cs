c7a44779ca1fd3b316c9cb5c20bbacb2d5cc7262
Jesse Wei <jesse.wei@activenetwork.com>
2019-09-08 22:41:16 +0800

    First commit.

---
+++ BTree/BTree/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace BTree
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.statusBarLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.menuFile = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuFile_Circle1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuFile_Circle2 = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuFile_Circle3 = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuFile_BTree = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuFile_Exit = new System.Windows.Forms.ToolStripMenuItem();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.menuStrip1.SuspendLayout();
+			this.statusStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFile});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(684, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusBarLabel1});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 489);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(684, 22);
+			this.statusStrip1.TabIndex = 1;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// statusBarLabel1
+			// 
+			this.statusBarLabel1.Name = "statusBarLabel1";
+			this.statusBarLabel1.Size = new System.Drawing.Size(29, 17);
+			this.statusBarLabel1.Text = "N/A";
+			// 
+			// menuFile
+			// 
+			this.menuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFile_Circle1,
+            this.menuFile_Circle2,
+            this.menuFile_Circle3,
+            this.menuFile_BTree,
+            this.menuFile_Exit});
+			this.menuFile.Name = "menuFile";
+			this.menuFile.Size = new System.Drawing.Size(37, 20);
+			this.menuFile.Text = "&File";
+			// 
+			// menuFile_Circle1
+			// 
+			this.menuFile_Circle1.Name = "menuFile_Circle1";
+			this.menuFile_Circle1.Size = new System.Drawing.Size(152, 22);
+			this.menuFile_Circle1.Text = "Circle &1";
+			this.menuFile_Circle1.Click += new System.EventHandler(this.menuFile_Circle1_Click);
+			// 
+			// menuFile_Circle2
+			// 
+			this.menuFile_Circle2.Name = "menuFile_Circle2";
+			this.menuFile_Circle2.Size = new System.Drawing.Size(152, 22);
+			this.menuFile_Circle2.Text = "Circle &2";
+			this.menuFile_Circle2.Click += new System.EventHandler(this.menuFile_Circle2_Click);
+			// 
+			// menuFile_Circle3
+			// 
+			this.menuFile_Circle3.Name = "menuFile_Circle3";
+			this.menuFile_Circle3.Size = new System.Drawing.Size(152, 22);
+			this.menuFile_Circle3.Text = "Circle &3";
+			this.menuFile_Circle3.Click += new System.EventHandler(this.menuFile_Circle3_Click);
+			// 
+			// menuFile_BTree
+			// 
+			this.menuFile_BTree.Name = "menuFile_BTree";
+			this.menuFile_BTree.Size = new System.Drawing.Size(152, 22);
+			this.menuFile_BTree.Text = "B-&Tree";
+			this.menuFile_BTree.Click += new System.EventHandler(this.menuFile_BTree_Click);
+			// 
+			// menuFile_Exit
+			// 
+			this.menuFile_Exit.Name = "menuFile_Exit";
+			this.menuFile_Exit.Size = new System.Drawing.Size(152, 22);
+			this.menuFile_Exit.Text = "E&xit";
+			this.menuFile_Exit.Click += new System.EventHandler(this.menuFile_Exit_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(0, 24);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(684, 465);
+			this.panel1.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(684, 511);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem menuFile;
+		private System.Windows.Forms.ToolStripMenuItem menuFile_Circle1;
+		private System.Windows.Forms.ToolStripMenuItem menuFile_Circle2;
+		private System.Windows.Forms.ToolStripMenuItem menuFile_Circle3;
+		private System.Windows.Forms.ToolStripMenuItem menuFile_BTree;
+		private System.Windows.Forms.ToolStripMenuItem menuFile_Exit;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel statusBarLabel1;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
+
---
+++ BTree/BTree/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BTree
+{
+	public partial class Form1 : Form
+	{
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private class Circle
+		{
+			public Point center;
+			public int radius;
+
+			public Circle(int x, int y, int r)
+				: this(new Point(x, y), r) {
+			}
+
+			public Circle(Point p, int r) {
+				center = p;
+				radius = r;
+			}
+		}
+
+		private List<Circle> GetCircles(int count = 1) {
+			var list = new List<Circle>();
+			int r = 30;
+			int interval = 20;
+			int distance = 2 * r + interval;
+			for (int i = 0; i < count; i++) {
+				list.Add(new Circle(r + interval / 2 + distance * i, r + interval / 2, r));
+			}
+			return list;
+		}
+
+		private void DrawCircles(Circle circle, Graphics g = null, Pen p = null) {
+			DrawCircles(new List<Circle>() { circle }, g, p);
+		}
+
+		private void DrawCircles(List<Circle> list, Graphics g = null, Pen p = null) {
+			panel1.Refresh();
+			if (g == null) {
+				g = GetGraphics();
+			}
+			if (p == null) {
+				p = GetPen(Color.FromArgb(0, 0, 0));
+			}
+			for (int i = 0; i < list.Count; i++) {
+				DrawCircle(list[i], (i + 1).ToString(), g, p);
+			}
+		}
+
+		private void DrawCircle(Circle c, String text, Graphics g = null, Pen p = null) {
+			if (c == null || c.radius <= 0) {
+				return;
+			}
+			if (g == null) {
+				g = GetGraphics();
+			}
+			if (p == null) {
+				p = GetPen();
+			}
+			g.DrawEllipse(p, c.center.X - c.radius, c.center.Y - c.radius, c.radius * 2, c.radius * 2);
+			//g.DrawEllipse(p, c.center.X, c.center.Y, 1, 1); // Center point
+			//g.FillEllipse(new SolidBrush(p.Color), c.center.X - c.radius, c.center.Y - c.radius, c.radius * 2, c.radius * 2);
+
+			Font font = new Font("Arial", 10);
+			g.DrawString(text, font, p.Brush, c.center.X - 5, c.center.Y - font.Height / 2);
+		}
+
+		private Graphics GetGraphics() {
+			var g = panel1.CreateGraphics();
+			g.SmoothingMode = SmoothingMode.AntiAlias;
+			return g;
+		}
+
+		private Pen GetPen(Color c = new Color(), int width = 1) {
+			var p = new Pen(c, width);
+			return p;
+		}
+
+		private void menuFile_Circle1_Click(object sender, EventArgs e) {
+			DrawCircles(GetCircles(), null, new Pen(Color.Red));
+		}
+
+		private void menuFile_Circle2_Click(object sender, EventArgs e) {
+			DrawCircles(GetCircles(2), null, new Pen(Color.Green));
+		}
+
+		private void menuFile_Circle3_Click(object sender, EventArgs e) {
+			DrawCircles(GetCircles(3), null, new Pen(Color.Purple));
+		}
+
+		private void menuFile_BTree_Click(object sender, EventArgs e) {
+			int r = 20;
+			int a = 20; // Distance between border of left and right leaves
+			int b = 30; // Distance between border of left leaf and the root.
+			int h = (int)Math.Round(Math.Sqrt(Math.Pow(b + 2 * r, 2) - Math.Pow(r + a / 2, 2)), 0);
+			Point A = new Point(2 * r + a / 2, r);
+			Point B = new Point(r, h + r);
+			Point C = new Point(3 * r + a, h + r);
+
+			int x0 = a / 2; // Left position from which drawing starts.
+			int y0 = x0; // Top position from which drawing starts.
+			Size offset = new Size(x0, y0);
+			A = Point.Add(A, offset);
+			B = Point.Add(B, offset);
+			C = Point.Add(C, offset);
+
+			List<Circle> list = new List<Circle>();
+			list.Add(new Circle(A, r));
+			list.Add(new Circle(B, r));
+			list.Add(new Circle(C, r));
+
+			var g = GetGraphics();
+			var pen = new Pen(Color.Blue);
+			DrawCircles(list, g, pen);
+			//g.DrawLines(pen, new Point[] { A, B, C, A });
+
+			int xb, yb; // Coordinate of point line crossing circle border.
+
+			// A-B at A's border
+			xb = (int)Math.Round((double)r / (b + 2 * r) * (B.X - A.X), 0) + A.X;
+			yb = (int)Math.Round((double)r / (b + 2 * r) * (B.Y - A.Y), 0) + A.Y;
+			var AB = new Point(xb, yb);
+
+			// A-B at B's border
+			xb = (int)Math.Round((double)r / (b + 2 * r) * (A.X - B.X), 0) + B.X;
+			yb = (int)Math.Round((double)r / (b + 2 * r) * (A.Y - B.Y), 0) + B.Y;
+			var BA = new Point(xb, yb);
+
+			// A-C at A's border
+			xb = (int)Math.Round((double)r / (b + 2 * r) * (C.X - A.X), 0) + A.X;
+			yb = (int)Math.Round((double)r / (b + 2 * r) * (C.Y - A.Y), 0) + A.Y;
+			var AC = new Point(xb, yb);
+
+			// A-C at C's border
+			xb = (int)Math.Round((double)r / (b + 2 * r) * (A.X - C.X), 0) + C.X;
+			yb = (int)Math.Round((double)r / (b + 2 * r) * (A.Y - C.Y), 0) + C.Y;
+			var CA = new Point(xb, yb);
+
+			g.DrawLine(pen, AB, BA);
+			g.DrawLine(pen, AC, CA);
+		}
+
+		private void menuFile_Exit_Click(object sender, EventArgs e) {
+			Application.Exit();
+		}
+	}
+}
